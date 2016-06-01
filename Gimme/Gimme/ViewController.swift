edf7ad2b553b24e892da0b70b5ababb228ff9408
Paul Gerlich <paul.gerlich@workiva.com>
2016-05-31 22:41:47 -0500

    Added initial project

---
+++ Gimme/Gimme/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Gimme
+//
+//  Created by Paul.Gerlich on 5/31/16.
+//  Copyright © 2016 Paul.Gerlich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+}
+
